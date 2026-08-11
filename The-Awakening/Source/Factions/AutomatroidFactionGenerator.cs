4a05014e5072d929f02cba8cf923087ee0a021df
ketamjne <koenverlysdonk@gmail.com>
2026-08-11 19:19:06 -0400

    Add AutomatroidFactionGenerator to ensure faction exists

---
+++ The-Awakening/Source/Factions/AutomatroidFactionGenerator.cs
@@ -0,0 +1,41 @@
+using RimWorld;
+using Verse;
+
+namespace TheAwakening
+{
+    [StaticConstructorOnStartup]
+    public static class AutomatroidFactionGenerator
+    {
+        static AutomatroidFactionGenerator()
+        {
+            LongEventHandler.ExecuteWhenFinished(EnsureAutomatroidFactionExists);
+        }
+
+        private static void EnsureAutomatroidFactionExists()
+        {
+            if (Find.FactionManager == null)
+                return;
+
+            FactionDef def = DefDatabase<FactionDef>.GetNamedSilentFail(
+                "DMS_Automatroid_Hostile"
+            );
+
+            if (def == null)
+            {
+                Log.Error("[The Awakening] DMS_Automatroid_Hostile FactionDef not found.");
+                return;
+            }
+
+            if (Find.FactionManager.AllFactions.Any(f => f.def == def))
+                return;
+
+            Faction faction = FactionGenerator.NewGeneratedFaction(def);
+            Find.FactionManager.Add(faction);
+
+            Log.Message(
+                "[The Awakening] Created hidden Automatroid faction: "
+                + faction.Name
+            );
+        }
+    }
+}
